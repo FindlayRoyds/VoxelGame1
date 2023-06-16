9c3bf6293ee34a1394edb7bcc3fbbe752b1bcf20
FindlayRoyds <findlayroyds@gmail.com>
2023-06-17 09:06:45 +1200

    Initial Commit

---
+++ VoxelGame1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VoxelGame1
+//
+//  Created by Findlay Royds on 17/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
